28472afa0660b63ec623177b82c12de14aeed012
lotfi80 <salioslim@hotmail.de>
2024-11-04 13:10:03 +0100

    chnaged links

--- src/main.ts
+++ src/main.ts
@@ -10,13 +10,13 @@ import cookieparser from 'cookie-parser';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-const clientUrl = `http://localhost:${PORT}`;
+// const clientUrl = `http://localhost:${PORT}`;
 
 const app = express();
 app.use(express.json());
 app.use(cookieparser());
 app.use(cors({
-  origin: 'http://localhost:5174', 
+  origin: '*', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true 
@@ -39,6 +39,6 @@ app.use('/timeEntry', authorizeJwt, entryTimeRouter);
   
     app.listen(PORT, () => {
       console.log(`The server 🙈 is listening on port ${PORT}`);
-      console.log(`Visit ${clientUrl} in your browser`);
+      // console.log(`Visit ${clientUrl} in your browser`);
     });
   })();
